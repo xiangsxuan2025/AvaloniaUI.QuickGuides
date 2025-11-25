f29e299eed3755ab668415bfdae73f405ebe17e9
xiangsxuan2025 <xiangshouxuan@foxmail.com>
2025-11-25 22:46:22 +0800

    docs: 阅读笔记, 关于Converters放在哪里的, 关于[ObservableProperty]
    这样，窗口内的所有控件, 都可以通过{StaticResource ResourceKey}来引用这些转换器。
    最佳实践建议
    单个窗口 使用 → Window.Resources
    整个应用 使用 → Application.Resources, 如：BooleanToVisibilityConverter 这种很通用的转换器
    特定用户控件 使用 → UserControl.Resources
    局部 使用 → 最近容器的Resources
    
    // 代码生成器,可以让通知属性更简洁. 来自CommunityToolkit.Mvvm.ComponentModel;
     [ObservableProperty]private int _age;

--- BindingsAndConverters/ViewModels/MainWindowViewModel.cs
+++ BindingsAndConverters/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace BindingConvertersExample.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    // 代码生成器,可以让通知属性更简洁
     [ObservableProperty] private int _age;
 }
